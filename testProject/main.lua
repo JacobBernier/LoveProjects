925a4678fa85845c7c89bfa475ee72f4c4857f5e
JacobBernier <112507264+JacobBernier@users.noreply.github.com>
2024-04-03 22:50:34 -0400

    end target game

--- testProject/main.lua
+++ testProject/main.lua
@@ -5,7 +5,8 @@ function love.load()
     target.radius = 50
 
     score = 0
-    timer = 10
+    timer = 0
+    gamestate = 1
 
     gameFont = love.graphics.newFont(40)
 
@@ -24,32 +25,43 @@ function love.update(dt)
 
     if timer < 0 then
         timer = 0
+        gamestate = 1
+        score = 0
     end
 end
 
 function love.draw()
     love.graphics.draw(sprites.sky, 0, 0)
 
-    love.graphics.setColor(1, 0.63, 0)
-    love.graphics.circle("fill", target.x, target.y, target.radius)
+    -- love.graphics.setColor(1, 0.63, 0)
+    -- love.graphics.circle("fill", target.x, target.y, target.radius)
 
     love.graphics.setColor(1, 1, 1)
     love.graphics.setFont(gameFont)
-    love.graphics.print(score, 0, 0)
-    love.graphics.print(math.ceil(timer), 300, 0)
+    love.graphics.print("Score: " .. score, 5, 5)
+    love.graphics.print("Time: " .. math.ceil(timer), 300, 5)
 
-    love.graphics.draw(sprites.target,target.x - target.radius, target.y - target.radius)
+    if gamestate == 1 then
+        love.graphics.printf("Click anywhere to begin!", 0, 250, love.graphics.getWidth(), "center")
+    end
+    if gamestate == 2 then
+        love.graphics.draw(sprites.target,target.x - target.radius, target.y - target.radius)
+    end
     love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
+
 end
 
 function love.mousepressed( x, y, button, istouch, presses )
-    if button == 1 then
+    if button == 1 and gamestate == 2 then
         local mouseToTarget = distanceBetween(x,y,target.x,target.y)
         if mouseToTarget < target.radius then
             score = score + 1
             target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
             target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
         end
+    elseif button == 1 and gamestate == 1 then
+        gamestate = 2
+        timer = 10
     end
 end
 
