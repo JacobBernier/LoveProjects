68cc826f0ec52ffb23070756e0df8cfed9928878
JacobBernier <112507264+JacobBernier@users.noreply.github.com>
2024-04-13 18:24:38 -0400

    Ajout du font et difficultés

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -19,12 +19,11 @@ function love.load()
     zombieSpeedRange = {}
     zombieSpeedRange.MinSpeed = 120
     zombieSpeedRange.MaxSpeed = 220
-
-
     
-
     gamestate = 1
     myFont = love.graphics.newFont(30)
+    FontDifficulties = love.graphics.newFont("Fonts/pixel-emulator-font/PixelEmulator-xq08.ttf", 30)
+
 
     maxTimeConst = 2
     maxTime = maxTimeConst
@@ -35,40 +34,75 @@ function love.load()
     bulletSpinChargeMaxTime = 4
     bulletSpinChargeTimer = bulletSpinChargeMaxTime
 
+    difficultyEasy = {}
+    difficultyMedium = {}
+    difficultyHard = {}
+
+    difficultyEasy.Name = "Easy"
+    difficultyMedium.Name = "Medium"
+    difficultyHard.Name = "Hard"
+
+    difficultyEasy.spawnSpeed = 1.5
+    difficultyMedium.spawnSpeed = 1
+    difficultyHard.spawnSpeed = 0.4
+
+    -- difficultyEasy.IncreaseSpeed = 0.95
+    -- difficultyMedium.IncreaseSpeed = 0.9
+    -- difficultyHard.IncreaseSpeed = 0.8
+
+    difficultyEasy.x = love.graphics.getWidth() - (love.graphics.getWidth() / 5 * 4)
+    difficultyMedium.x = love.graphics.getWidth() - (love.graphics.getWidth() / 2)
+    difficultyHard.x = love.graphics.getWidth() - (love.graphics.getWidth() / 5)
+    
+    difficultyEasy.y = love.graphics.getHeight() - (love.graphics.getHeight() / 3)
+    difficultyMedium.y = love.graphics.getHeight() - (love.graphics.getHeight() / 3)
+    difficultyHard.y = love.graphics.getHeight() - (love.graphics.getHeight() / 3)
+
+    difficultyEasy.zombie = spawnSpecificZombie(difficultyEasy.x, difficultyEasy.y)
+    difficultyMedium.zombie = spawnSpecificZombie(difficultyMedium.x, difficultyMedium.y)
+    difficultyHard.zombie = spawnSpecificZombie(difficultyHard.x, difficultyHard.y)
+
+    difficulties = {difficultyEasy, difficultyMedium, difficultyHard}
+    SelectedDifficulty = difficultyEasy
+
 end
 
 function love.update(dt)
-    if gamestate == 2 then
-        if love.keyboard.isDown('d') and player.x < love.graphics.getWidth() then
-            player.x = player.x + player.speed*dt
-        end
-        if love.keyboard.isDown('a') and player.x > 0 then
-            player.x = player.x - player.speed*dt
-        end
-        if love.keyboard.isDown('w') and player.y > 0 then
-            player.y = player.y - player.speed*dt
-        end
-        if love.keyboard.isDown('s') and player.y < love.graphics.getHeight() then
-            player.y = player.y + player.speed*dt
-        end
+    if love.keyboard.isDown('d') and player.x < love.graphics.getWidth() then
+        player.x = player.x + player.speed*dt
+    end
+    if love.keyboard.isDown('a') and player.x > 0 then
+        player.x = player.x - player.speed*dt
+    end
+    if love.keyboard.isDown('w') and player.y > 0 then
+        player.y = player.y - player.speed*dt
+    end
+    if love.keyboard.isDown('s') and player.y < love.graphics.getHeight() then
+        player.y = player.y + player.speed*dt
     end
 
-    for i,z in ipairs(zombies) do
-        z.x = z.x + (math.cos(zombiePlayerAngle(z)) * z.speed * dt)
-        z.y = z.y + (math.sin(zombiePlayerAngle(z)) * z.speed * dt)
 
-        if distanceBetween(z.x, z.y, player.x, player.y) < 30 then
-            for i,z in ipairs(zombies) do
-                zombies[i] = nil
-                gamestate = 1
-                player.x = love.graphics.getWidth()/2
-                player.y = love.graphics.getHeight()/2
-                if highscore < score then
-                    highscore = score
+    if gamestate == 2 then
+        for i,z in ipairs(zombies) do
+            z.x = z.x + (math.cos(zombiePlayerAngle(z)) * z.speed * dt)
+            z.y = z.y + (math.sin(zombiePlayerAngle(z)) * z.speed * dt)
+    
+            if distanceBetween(z.x, z.y, player.x, player.y) < 30 then
+                for i,z in ipairs(zombies) do
+                    zombies[i] = nil
+                    gamestate = 1
+                    player.x = love.graphics.getWidth()/2
+                    player.y = love.graphics.getHeight()/2
+                    if highscore < score then
+                        highscore = score
+                    end
                 end
             end
         end
     end
+    
+
+    
 
     for i,b in ipairs(bullets) do
         b.x = b.x + (math.cos(b.direction) * b.speed * dt)
@@ -143,11 +177,11 @@ function love.update(dt)
         timer = timer - dt
         if timer <= 0 then
             spawnZombie()
-            if maxTime > 0.75 then
-                maxTime = 0.9 * maxTime
+            if maxTime > 0.5 then
+                maxTime = 0.95 * maxTime
                 timer = maxTime
             else 
-                maxTime = maxTime - 0.005
+                maxTime = maxTime - 0.003
                 timer = maxTime
             end
             
@@ -160,7 +194,21 @@ function love.update(dt)
         end
     end
 
-
+    if gamestate == 1 then
+        for i,d in ipairs(difficulties) do
+            for g,b in ipairs(bullets) do
+                if distanceBetween(d.x, d.y, b.x, b.y) < 20 then
+                    SelectedDifficulty = d
+                    b.dead = true
+                    newGame()
+                    for i=#bullets,1,-1 do
+                        local b = bullets[i]
+                        table.remove(bullets, i)
+                    end
+                end
+            end
+        end
+    end
 end
 
 
@@ -169,8 +217,20 @@ function love.draw()
     love.graphics.draw(sprites.background,0,0)
 
     if gamestate == 1 then
-        love.graphics.setFont(myFont)
-        love.graphics.printf("Click anywhere to begin!", 0, 50, love.graphics.getWidth(), "center")
+        -- love.graphics.setFont(myFont)
+        love.graphics.setFont(FontDifficulties)
+
+        love.graphics.printf("Select your difficulty!", 0, 50, love.graphics.getWidth(), "center")
+
+        love.graphics.printf("Left click to shoot, right click for wave bullets", 0, 100, love.graphics.getWidth(), "center")
+
+
+        for i,d in ipairs(difficulties) do
+            love.graphics.draw(sprites.zombie, d.x, d.y, zombiePlayerAngle(d), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
+            local textWidth = love.graphics.getFont():getWidth(d.Name)
+            local centerX = d.x - textWidth / 2
+            love.graphics.printf(d.Name, centerX, d.y + 40, love.graphics.getWidth())
+        end
     end
     love.graphics.printf("Score: " .. score, 0, love.graphics.getHeight() - 100, love.graphics.getWidth(), "center")
     love.graphics.printf("Highscore: " .. highscore, 0, love.graphics.getHeight() - 50, love.graphics.getWidth(), "center")
@@ -180,17 +240,19 @@ function love.draw()
     end
 
 
-    love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
-
-    for i,z in ipairs(zombies) do
-        love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
-    end
+    
 
     for i,b in ipairs(bullets) do
         love.graphics.draw(sprites.bullet, b.x, b.y, b.direction, 0.25, 0.1, sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2)
     end
     for i,bs in ipairs(bulletsSpin) do
         love.graphics.draw(sprites.bullet, bs.x, bs.y, bs.direction, 0.25, 0.1, sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2)
+    end
+
+    love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+
+    for i,z in ipairs(zombies) do
+        love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
     end
 end
 
@@ -202,22 +264,26 @@ function love.keypressed(key)
     end
 end
 
+function newGame()
+    gamestate = 2
+    maxTime = SelectedDifficulty.spawnSpeed
+    timer = maxTime
+    score = 0
+    bulletSpinCharges = 0
+    bulletSpinChargeMaxTime = 4
+    bulletSpinChargeTimer = bulletSpinChargeMaxTime
+end
+
 function love.mousepressed( x, y, button )
-    if button == 1 and gamestate == 2 then
+    if button == 1 then
         spawnBullet()
-    elseif button == 1 and gamestate == 1 then
-        gamestate = 2
-        maxTime = maxTimeConst
-        timer = maxTime
-        score = 0
-        bulletSpinCharges = 0
-        bulletSpinChargeMaxTime = 4
-        bulletSpinChargeTimer = bulletSpinChargeMaxTime
     end
 
     if button == 2 and gamestate == 2 and bulletSpinCharges > 0 then
         spawnBulletSpin()
         bulletSpinCharges = bulletSpinCharges - 1
+    elseif button == 2 and gamestate == 1 then
+        spawnBulletSpin()
     end
 end
 
@@ -256,6 +322,18 @@ function spawnZombie()
     table.insert(zombies, zombie)
 end
 
+function spawnSpecificZombie(x, y)
+    local zombie = {}
+    zombie.x = x
+    zombie.y = y
+    --zombie.speed = math.random(zombieSpeedRange.MinSpeed, zombieSpeedRange.MaxSpeed)
+    zombie.dead = false
+
+    
+
+    return zombie
+end
+
 function spawnBullet()
     local bullet = {}
     bullet.x = player.x
@@ -274,7 +352,7 @@ function spawnBulletSpin()
     bulletSpin.direction = playerMouseAngle()
     bulletSpin.time = 0
     bulletSpin.amplitude = 20
-    bulletSpin.frequency = 40
+    bulletSpin.frequency = 80
 
     table.insert(bulletsSpin, bulletSpin)
 end
